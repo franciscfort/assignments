9171932af320ea8ddd0bc4c08e7e8a69542423a0
franciscfort <114653597+franciscfort@users.noreply.github.com>
2022-10-27 17:40:35 +0100

    Create register.php

---
+++ week 4/register.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_POST['submit'])){
+    $username = $_POST['fullname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+registerUser($username, $email, $password);
+
+}
+
+function registerUser($username, $email, $password){ $form_data = array(
+       'fullname' => $username,
+       'email' => $email,
+       'password' => $password );
+   $exist = checkIfUserExist($email);
+   if($exist){
+       echo "User already exist";
+   }else{
+       $file = fopen('../storage/users.csv', 'a');
+       fputcsv($file, $form_data);
+       fclose($file);
+       echo "User registered";
+   }}
+    //save data into the file
+function checkIfUserExist($email){
+       $file = fopen('../storage/users.csv', 'r');
+       while(!feof($file)){
+           $line = fgetcsv($file);
+           if($line[1] == $email){
+               return true;
+           }
+       }
+       fclose($file);
+       return false;
+   }
+    
